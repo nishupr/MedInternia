8b2c252a86be0fa104280e8138af6e66a2ee2209
Jayam Srivastava <jayamsrivastava99@gmail.com>
2026-07-12 17:39:13 +0530

    Sec :  IP-based rate limiters can misbehave or crash behind a reverse proxy#572

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,6 +25,7 @@ process.on('uncaughtException', (error: Error) => {
 });
 
 const app: Application = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 // Initialize application
